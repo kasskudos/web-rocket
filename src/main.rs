bc92abe17865a530f84024a3bb3f1cbef3565a08
kasskudos <150641232+kasskudos@users.noreply.github.com>
2023-11-13 01:48:28 -0300

    Add MySql (#1)
    Co-authored-by: Lucas Civali <lucascivali@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,36 @@
-use actix_web::{App, HttpServer, Responder};
+use actix_web::{App, get, HttpResponse, HttpServer, Responder};
 use std::env;
 use crate::routes::{healthz, ping};
 
+use mysql::prelude::*;
+
+extern crate mysql;
+
 mod routes;
 
+
+#[get("/show")]
+async fn showTables() -> HttpResponse {
+
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL not set");
+
+    let builder = mysql::OptsBuilder::from_opts(mysql::Opts::from_url(&database_url).unwrap());
+    let pool = mysql::Pool::new(builder.ssl_opts(mysql::SslOpts::default())).unwrap();
+
+    let mut conn = pool.get_conn().expect("Failed to get connection from pool");
+
+
+    // Run the SHOW TABLES query
+    let tables: Vec<String> = conn
+        .query_map("SHOW TABLES", |row: mysql::Row| {
+            let table_name: String = mysql::from_row(row);
+            table_name
+        })
+        .expect("Failed to execute query");
+
+    HttpResponse::Ok().body(tables.join(","))
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -15,6 +42,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(healthz)
             .service(ping)
+            .service(showTables)
     })
         .bind((address, port))?
         .run()
